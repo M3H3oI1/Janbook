60096d4d3385c18b42a18e8b97b1349d8219e159
emam10mehedi <imranhasan3955000@gmail.com>
2023-11-23 03:13:32 +0600

    \

--- src/components/Leftbar.jsx
+++ src/components/Leftbar.jsx
@@ -9,7 +9,7 @@ function Leftbar() {
                         <Link to={"/Profile"} className="flex gap-3 py-2 pl-3 hover:bg-gray-300 rounded-lg">
                             <div className="avatar ">
                                 <div className="w-10 rounded-full ">
-                                    <img src="/public/logo/profile.jpeg" />
+                                    <img src="/logo/profile.jpeg" />
                                 </div>
                             </div>
                             <p className=" font-bold mt-3">Emam Mehedi</p>
@@ -18,35 +18,35 @@ function Leftbar() {
 
                     <li>
                         <a href="" className="flex gap-3 py-2 pl-3 hover:bg-gray-300 rounded-lg">
-                            <img src="/public/icon/friend.png" className="h-10 w-10" alt="" />
+                            <img src="/icon/friend.png" className="h-10 w-10" alt="" />
                             <p className="font-bold mt-2">Find Friends</p>
                         </a>
                     </li>
 
                     <li>
                         <a href="" className="flex gap-3 py-2 pl-3 hover:bg-gray-300 rounded-lg">
-                            <img src="/public/icon/memories.png" className="h-10 w-10" alt="" />
+                            <img src="/icon/memories.png" className="h-10 w-10" alt="" />
                             <p className="font-bold mt-2">Memories</p>
                         </a>
                     </li>
 
                     <li>
                         <a href="" className="flex gap-3 py-2 pl-3 hover:bg-gray-300 rounded-lg">
-                            <img src="/public/icon/saved.png" className="h-10 w-10" alt="" />
+                            <img src="/icon/saved.png" className="h-10 w-10" alt="" />
                             <p className="font-bold mt-2">Saved</p>
                         </a>
                     </li>
 
                     <li>
                         <a href="" className="flex gap-3 py-2 pl-3 hover:bg-gray-300 rounded-lg">
-                            <img src="/public/icon/messen.png" className="h-10 w-10" alt="" />
+                            <img src="/icon/messen.png" className="h-10 w-10" alt="" />
                             <p className="font-bold mt-2 ">Messenger</p>
                         </a>
                     </li>
 
                     <li>
                         <a href="" className="flex gap-3 py-2 pl-3 hover:bg-gray-300 rounded-lg">
-                            <img src="/public/icon/groups.png" className="h-10 w-10" alt="" />
+                            <img src="/icon/groups.png" className="h-10 w-10" alt="" />
                             <p className="font-bold mt-2">Groups</p>
                         </a>
                     </li>
@@ -54,7 +54,7 @@ function Leftbar() {
 
                     <li>
                         <a href="" className="flex gap-3 py-2 pl-3 hover:bg-gray-300 rounded-lg">
-                            <img src="/public/icon/video.png" className="h-10 w-10" alt="" />
+                            <img src="/icon/video.png" className="h-10 w-10" alt="" />
                             <p className="font-bold mt-2">Video</p>
                         </a>
                     </li>
@@ -68,13 +68,13 @@ function Leftbar() {
                         <ul className="p-4 rounded-lg">
                         <li>
                                 <a href="" className="flex gap-3 py-2  hover:bg-gray-300 rounded-lg">
-                                    <img src="/public/icon/calendar.png" className="h-10 w-10" alt="" />
+                                    <img src="/icon/calendar.png" className="h-10 w-10" alt="" />
                                     <p className="font-bold mt-2">Event</p>
                                 </a>
                             </li>
                             <li>
                                 <a href="" className="flex gap-3 py-2  hover:bg-gray-300 rounded-lg">
-                                    <img src="/public/icon/feed.png" className="h-10 w-10" alt="" />
+                                    <img src="/icon/feed.png" className="h-10 w-10" alt="" />
                                     <p className="font-bold mt-2">Feeds</p>
                                 </a>
                             </li>
@@ -82,28 +82,28 @@ function Leftbar() {
                                 
                             <li>
                                 <a href="" className="flex gap-3 py-2  hover:bg-gray-300 rounded-lg">
-                                    <img src="/public/icon/gaming.png" className="h-10 w-10" alt="" />
+                                    <img src="/icon/gaming.png" className="h-10 w-10" alt="" />
                                     <p className="font-bold mt-2">Gaming Video</p>
                                 </a>
                             </li>
 
                             <li >
                                 <a href="" className="flex gap-3 py-2  hover:bg-gray-300 rounded-lg">
-                                    <img src="/public/icon/MARKET.png" className="h-10 w-10" alt="" />
+                                    <img src="/icon/MARKET.png" className="h-10 w-10" alt="" />
                                     <p className="font-bold mt-2">Market Place</p>
                                 </a>
                             </li>
 
                             <li >
                                 <a href="" className="flex gap-3 py-2  hover:bg-gray-300 rounded-lg">
-                                    <img src="/public/icon/kids.png" className="h-10 w-10" alt="" />
+                                    <img src="/icon/kids.png" className="h-10 w-10" alt="" />
                                     <p className="font-bold mt-2">Messenger Kids</p>
                                 </a>
                             </li>
 
                             <li>
                                 <a href="" className="flex gap-3 py-2  hover:bg-gray-300 rounded-lg">
-                                    <img src="/public/icon/page.png" className="h-10 w-10" alt="" />
+                                    <img src="/icon/page.png" className="h-10 w-10" alt="" />
                                     <p className="font-bold mt-2">Pages</p>
                                 </a>
                             </li>
@@ -120,7 +120,7 @@ function Leftbar() {
                     <li>
                         <a className="avatar flex gap-2 py-2  hover:bg-gray-300 rounded-lg">
                             <div className="w-10 rounded-xl mx-3">
-                                <img src="/public/icon/valo.jpg" />
+                                <img src="/icon/valo.jpg" />
                             </div>
                             <p className=" font-semibold text-sm mt-3 ">Valorant Gameplay Bangladesh</p>
                         </a>
@@ -128,7 +128,7 @@ function Leftbar() {
                     <li>
                         <a className="avatar flex gap-2 py-2  hover:bg-gray-300 rounded-lg">
                             <div className="w-10 rounded-xl mx-3">
-                                <img src="/public/icon/react.png" />
+                                <img src="/icon/react.png" />
                             </div>
                             <p className=" font-semibold text-sm mt-3 ">React js Community</p>
                         </a>
@@ -136,7 +136,7 @@ function Leftbar() {
                     <li>
                         <a className="avatar flex gap-2 py-2  hover:bg-gray-300 rounded-lg">
                             <div className="w-10 rounded-xl mx-3">
-                                <img src="/public/icon/lara.jpeg" />
+                                <img src="/icon/lara.jpeg" />
                             </div>
                             <p className=" font-semibold text-sm mt-3 ">Php Laravel With Framework</p>
                         </a>
@@ -144,7 +144,7 @@ function Leftbar() {
                     <li>
                         <a className="avatar flex gap-2 py-2  hover:bg-gray-300 rounded-lg">
                             <div className="w-10 rounded-xl mx-3">
-                                <img src="/public/icon/valo.jpg" />
+                                <img src="/icon/valo.jpg" />
                             </div>
                             <p className=" font-semibold text-sm mt-3 ">Valorant Gameplay</p>
                         </a>
