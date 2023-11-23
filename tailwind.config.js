c374871912f9fde5e17768b2d952a7da20c41f28
emam10mehedi <imranhasan3955000@gmail.com>
2023-11-23 20:06:33 +0600

    /

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
-import daisyui from 'daisyui';
+import keepPreset from "keep-react/src/keep-preset.js";
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/keep-react/**/*.{js,jsx,ts,tsx}"
   ],
+  presets: [keepPreset],
+
   theme: {
     extend: {},
   },
-  plugins: [daisyui],
+  plugins: [],
 }
 
